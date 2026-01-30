bd9d1baee186e8a379199afa865f1d148cca72d7
Yosra Mohsen <yosramohsen2@gmail.com>
2026-01-30 18:08:40 +0200

    add icons to App assets

---
+++ lib/core/constants/app_assets.dart
@@ -0,0 +1,33 @@
+class AppAssets {
+  AppAssets._();
+
+  static const String _imagesPath = 'assets/images';
+  static const String _animationsPath = 'assets/animations';
+
+  // Social & Contact Icons
+  static const String github = '$_imagesPath/github.png';
+  static const String linkedin = '$_imagesPath/linkedin.png';
+  static const String whatsapp = '$_imagesPath/whatsapp.png';
+  static const String message = '$_imagesPath/message.png';
+
+  // Navigation & UI Icons
+  static const String mobile = '$_imagesPath/mobile.png';
+  static const String arrow = '$_imagesPath/arrow.png';
+  static const String drawer = '$_imagesPath/drawer.png';
+  static const String pallet = '$_imagesPath/pallet.png';
+  
+  // Theme Icons
+  static const String dark = '$_imagesPath/dark.png';
+  static const String light2 = '$_imagesPath/light (2).png'; // Consider renaming this file
+
+  // Numbered Icons (New Batch)
+  static const String icon6 = '$_imagesPath/Icon-6.png';
+  static const String icon10 = '$_imagesPath/Icon-10.png';
+  static const String icon11 = '$_imagesPath/Icon-11.png';
+  static const String icon12 = '$_imagesPath/Icon-12.png';
+  static const String icon13 = '$_imagesPath/Icon-13.png';
+  static const String icon14 = '$_imagesPath/Icon-14.png';
+  static const String icon15 = '$_imagesPath/Icon-15.png';
+  static const String icon16 = '$_imagesPath/Icon-16.png';
+  static const String icon17 = '$_imagesPath/Icon-17.png';
+}
