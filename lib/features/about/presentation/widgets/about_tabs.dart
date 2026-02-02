98789d5f3232389f748e4fcca27f943367a57f72
Yosra Mohsen <yosramohsen2@gmail.com>
2026-02-02 18:48:50 +0200

    feat: add hover effect to AboutTabs items

--- lib/features/about/presentation/widgets/about_tabs.dart
+++ lib/features/about/presentation/widgets/about_tabs.dart
@@ -3,7 +3,7 @@ import 'package:portfolio/core/theme/app_colors.dart';
 import 'package:portfolio/core/theme/app_dimensions.dart';
 import 'package:portfolio/core/theme/app_typography.dart';
 
-class AboutTabs extends StatelessWidget {
+class AboutTabs extends StatefulWidget {
   final int selectedIndex;
   final Function(int) onTabSelected;
   final List<String> tabs;
@@ -14,6 +14,13 @@ class AboutTabs extends StatelessWidget {
     required this.onTabSelected,
     required this.tabs,
   });
+
+  @override
+  State<AboutTabs> createState() => _AboutTabsState();
+}
+
+class _AboutTabsState extends State<AboutTabs> {
+  int? _hoveredIndex;
 
   @override
   Widget build(BuildContext context) {
@@ -27,7 +34,7 @@ class AboutTabs extends StatelessWidget {
 
     return Container(
       width: isMobile ? double.infinity : 400,
-      height: 56, // Slightly taller for better touch target and spacing
+      height: 56,
       padding: const EdgeInsets.all(AppDimensions.spacingMd),
       decoration: BoxDecoration(
         color: bgColors.primaryDefault,
@@ -38,27 +45,43 @@ class AboutTabs extends StatelessWidget {
         ),
       ),
       child: Row(
-        children: List.generate(tabs.length, (index) {
-          final isSelected = selectedIndex == index;
+        children: List.generate(widget.tabs.length, (index) {
+          final isSelected = widget.selectedIndex == index;
+          final isHovered = _hoveredIndex == index;
+
+          // Determine background color based on state
+          Color itemColor = Colors.transparent;
+          if (isSelected) {
+            itemColor = bgColors.brandSolid;
+          } else if (isHovered) {
+            itemColor = bgColors.primaryHover;
+          } else {
+            itemColor = bgColors.primarySecondary;
+          }
+
           return Expanded(
-            child: GestureDetector(
-              onTap: () => onTabSelected(index),
-              child: AnimatedContainer(
-                duration: const Duration(milliseconds: 300),
-                curve: Curves.easeInOut,
-                margin: const EdgeInsets.symmetric(horizontal: 2),
-                decoration: BoxDecoration(
-                  color: isSelected ? bgColors.brandSolid : bgColors.primarySecondary,
-                  borderRadius: BorderRadius.circular(AppDimensions.radius2xl),
-                ),
-                alignment: Alignment.center,
-                child: Text(
-                  tabs[index],
-                  style: AppTypography.labelMd(
-                    color: isSelected
-                        ? textColors.primaryToggle 
-                        : textColors.primaryDisabledToggle,
-                    fontWeight: isSelected ? FontWeight.w700 : FontWeight.w500,
+            child: MouseRegion(
+              onEnter: (_) => setState(() => _hoveredIndex = index),
+              onExit: (_) => setState(() => _hoveredIndex = null),
+              child: GestureDetector(
+                onTap: () => widget.onTabSelected(index),
+                child: AnimatedContainer(
+                  duration: const Duration(milliseconds: 200),
+                  curve: Curves.easeInOut,
+                  margin: const EdgeInsets.symmetric(horizontal: 2),
+                  decoration: BoxDecoration(
+                    color: itemColor,
+                    borderRadius: BorderRadius.circular(AppDimensions.radius2xl),
+                  ),
+                  alignment: Alignment.center,
+                  child: Text(
+                    widget.tabs[index],
+                    style: AppTypography.labelMd(
+                      color: isSelected
+                          ? textColors.primaryToggle 
+                          : textColors.primaryDisabledToggle,
+                      fontWeight: isSelected ? FontWeight.w700 : FontWeight.w500,
+                    ),
                   ),
                 ),
               ),
