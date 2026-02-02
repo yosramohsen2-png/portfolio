5f9a4f2b1a64e36a59876829948d8f36bad291c0
Yosra Mohsen <yosramohsen2@gmail.com>
2026-02-02 13:24:13 +0200

    fix: add missing animate_do import in about_hero

--- lib/features/about/presentation/widgets/about_hero.dart
+++ lib/features/about/presentation/widgets/about_hero.dart
@@ -1,5 +1,6 @@
 import 'dart:math' as math;
 import 'package:flutter/material.dart';
+import 'package:animate_do/animate_do.dart';
 import 'package:portfolio/core/theme/app_colors.dart';
 import 'package:portfolio/core/theme/app_dimensions.dart';
 import 'package:portfolio/core/theme/app_typography.dart';
